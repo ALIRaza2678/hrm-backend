38fb62d44e0bd8160b75be167f7163fee5be725a
ALIRaza2678 <alizwl823@gmail.com>
2026-02-11 14:25:38 +0500

    UPDATE

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
 
     await mongoose.connect(mongoURI);
 
-    console.log(`✅ MongoDB Connected (${process.env.NODE_ENV})`);
+    console.log(`✅ MongoDB Connected `);
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
